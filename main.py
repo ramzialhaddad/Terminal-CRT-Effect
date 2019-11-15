ed94a487f0b97232169418e7a05bb8f75d652fe3
Ramzi Al Haddad <ramzialhaddadtm@gmail.com>
2019-11-15 01:04:39 -0600

    Added a whole bunch of stuffs

--- main.py
+++ main.py
@@ -74,9 +74,168 @@ def refreshScreen(display):
 			else:
 				print("    ", end="")
 				sys.stdout.flush()
-		print("\n")
+		print("\n",end="")
 
-refreshScreen(blank)
-refreshScreen(box)
-refreshScreen(E)
-refreshScreen(message)
+
+
+
+def boxGettingLarger():
+	system(clear_word)
+	for lines in range(30):
+#----------------------------------------------------------- start
+		for pixel in [1,1,1,1,1,1,1,1,1,1]:
+			sleep(1/hertz)
+			if pixel == 1:
+				print("\u2588\u2588\u2588\u2588", end="")
+				sys.stdout.flush()
+			else:
+				print("    ", end="")
+				sys.stdout.flush()
+		print("\n",end="")
+#-------------------------------------------------------------
+		for line in range(lines):
+			for pixel in [1,0,0,0,0,0,0,0,0,1]:
+				sleep(1/hertz)
+				if pixel == 1:
+					print("\u2588\u2588\u2588\u2588", end="")
+					sys.stdout.flush()
+				else:
+					print("    ", end="")
+					sys.stdout.flush()
+			print("\n",end="")
+#------------------------------------------------------------ end
+		for pixel in [1,1,1,1,1,1,1,1,1,1]:
+			sleep(1/hertz)
+			if pixel == 1:
+				print("\u2588\u2588\u2588\u2588", end="")
+				sys.stdout.flush()
+			else:
+				print("    ", end="")
+				sys.stdout.flush()
+		print("\n",end="")
+		system(clear_word)
+
+def tallerTower():
+	system(clear_word)
+	for lines in range(30):
+#----------------------------------------------------------- start
+		for pixel in [1,1,1,1,1,1,1,1,1,1]:
+			sleep(1/hertz)
+			if pixel == 1:
+				print("\u2588\u2588\u2588\u2588", end="")
+				sys.stdout.flush()
+			else:
+				print("    ", end="")
+				sys.stdout.flush()
+		print("\n",end="")
+#-------------------------------------------------------------
+		for line in range(lines):
+			for pixel in [1,0,0,0,0,0,0,0,0,1]:
+				sleep(1/hertz)
+				if pixel == 1:
+					print("\u2588\u2588\u2588\u2588", end="")
+					sys.stdout.flush()
+				else:
+					print("    ", end="")
+					sys.stdout.flush()
+			print("\n",end="")
+#------------------------------------------------------------ end
+		for pixel in [1,1,1,1,1,1,1,1,1,1]:
+			sleep(1/hertz)
+			if pixel == 1:
+				print("\u2588\u2588\u2588\u2588", end="")
+				sys.stdout.flush()
+			else:
+				print("    ", end="")
+				sys.stdout.flush()
+		print("\n",end="")
+
+def smallBoxGettingLarger():
+	system(clear_word)
+	for lines in range(30):
+#----------------------------------------------------------- start
+		for pixel in [1,1,1,1,1]:
+			#sleep(1/hertz)
+			if pixel == 1:
+				print("\u2588\u2588\u2588\u2588", end="")
+				sys.stdout.flush()
+			else:
+				print("    ", end="")
+				sys.stdout.flush()
+		print("\n",end="")
+#-------------------------------------------------------------
+		for line in range(lines):
+			for pixel in [1,0,0,0,1]:
+				#sleep(1/hertz)
+				if pixel == 1:
+					print("\u2588\u2588\u2588\u2588", end="")
+					sys.stdout.flush()
+				else:
+					print("    ", end="")
+					sys.stdout.flush()
+			print("\n",end="")
+#------------------------------------------------------------ end
+		for pixel in [1,1,1,1,1]:
+			#sleep(1/hertz)
+			if pixel == 1:
+				print("\u2588\u2588\u2588\u2588", end="")
+				sys.stdout.flush()
+			else:
+				print("    ", end="")
+				sys.stdout.flush()
+		print("\n",end="")
+		system(clear_word)
+
+def movingLargerSmallBox():
+	system(clear_word)
+	for lines in range(30):
+#----------------------------------------------------------- start
+		for space in range(lines):
+			print("    ", end="")
+			sys.stdout.flush()
+		for pixel in [1,1,1,1,1]:
+			#sleep(1/hertz)
+			if pixel == 1:
+				print("\u2588\u2588\u2588\u2588", end="")
+				sys.stdout.flush()
+			else:
+				print("    ", end="")
+				sys.stdout.flush()
+		print("\n",end="")
+#-------------------------------------------------------------
+		for line in range(lines):
+			for space in range(lines):
+				print("    ", end="")
+				sys.stdout.flush()
+			for pixel in [1,0,0,0,1]:
+				#sleep(1/hertz)
+				if pixel == 1:
+					print("\u2588\u2588\u2588\u2588", end="")
+					sys.stdout.flush()
+				else:
+					print("    ", end="")
+					sys.stdout.flush()
+			print("\n",end="")
+#------------------------------------------------------------ end
+		for space in range(lines):
+				print("    ", end="")
+				sys.stdout.flush()
+		for pixel in [1,1,1,1,1]:
+			#sleep(1/hertz)
+			if pixel == 1:
+				print("\u2588\u2588\u2588\u2588", end="")
+				sys.stdout.flush()
+			else:
+				print("    ", end="")
+				sys.stdout.flush()
+		print("\n",end="")
+		system(clear_word)
+
+#refreshScreen(blank)
+#refreshScreen(box)
+#refreshScreen(E)
+#refreshScreen(message)
+#special()
+#tallerTower()
+#smallBoxGettingLarger()
+movingLargerSmallBox()
